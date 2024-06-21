7c13110bbd4276eb82153644651ebfdf529d7cea
Unique Jain <unique.21bcon647@jecrcu.edu.in>
2024-06-21 14:24:00 +0000

    Variables & DataType

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail = "unique@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountID = 2 // not allowed
+accountEmail = "uj@gmail.com"
+accountPassword = "21212121"
+a1ccountCity ="Bengaluru"
+
+console.log(accountID);
+// console.log(accountEmail);
+
+console.table(accountID, accountEmail, accountPassword, accountCity);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert("Hello")
+// alert(3 + 3)
+// We are using node.js not browser
+
+console.log(3 
+    + 3); // code readabililty shoud be high
+
+console.log("unique")
+
+let name = "unique"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// Primitive data types
+// number => 2 to power 53
+// Bigint
+// String => ""
+// boolean = true/false
+// null => standalone value
+// undefined =>
+// symbol => unique 
+
+
+// object
+
+console.log(typeof "unique"); // output is String
+console.log(typeof undefined); // output is Undefined
+console.log(typeof null); // output is object
+
