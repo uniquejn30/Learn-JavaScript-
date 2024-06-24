a0c684359bd6691141820e6643656afc9d14b2d7
Unique Jain <unique.21bcon647@jecrcu.edu.in>
2024-06-24 13:01:25 +0000

    Operation and Comparision in JS

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,33 +1,81 @@
+// ********************** Conversions **********************
 // let score = "33abc"
 // let score = null
 // let score = undefined
-let score = true
+// let score = true
 
 // const {score} = req.body
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 //null => NaN
 // true => 1 or false => 0
 
-let isLoggedIn = "unique"
+// let isLoggedIn = "unique"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "Unique" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ************************ Operations ***************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2); 
+// console.log(2*2); 
+// console.log(2**3); 
+// console.log(2/3); 
+// console.log(2%3); 
+
+// let str1 ="Hello"
+// let str2 = " Unique"
+// let str3 = str1 + str2
+// console.log(str3)
+
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(1 + 2)
+
+
+// console.log( (3+4) * 5 % 3);
+
+// console.log(true);
+// console.log(+"");
+// console.log(+true);  
+// console.log(true+);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+    
+
+// + true => 1; true+ => Error
