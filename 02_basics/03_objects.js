30b63c89a23a48e0f9214449d3a76e65dad7a8f2
Unique Jain <unique.21bcon647@jecrcu.edu.in>
2024-06-27 14:44:45 +0000

    Object in JS

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,60 @@
+// singleton used in only constructor 
+// Object.create
+
+// In Arrays :-
+// myArray = ["U" ," n"]
+// myArray[1]
+
+// Object literals
+// Use :-  Key , Values 
+
+const mySym = Symbol("Key1")
+
+
+
+
+const JsUser = {
+    name: "Unique",
+    "full name" : "Unique Jain",
+    // mySym: "mykey1",
+    [mySym]: "mykey1",
+    age: 21,
+    location: "Jaipur",
+    email: "uniquejn3004gmail.com",
+    isLoggedIn : false,
+    lastLoginDays: ["Monday", "Saturedy"]
+
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser.mySym);
+// console.log(typeof JsUser.mySym);
+// console.log(JsUser[mySym]);
+
+
+// JsUser.email = "unique@chatgpt.com"
+// Object.freeze(JsUser)
+// JsUser.email = "unique@microsoft.com"
+// console.log(JsUser);
+
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+    
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+    
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
+
+
+
+
+
