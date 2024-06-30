6e75fe660b6f8ce3ba42c85e6b04ea75e9f76d0a
Unique Jain <unique.21bcon647@jecrcu.edu.in>
2024-06-30 12:39:39 +0000

    IIFE in JS

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai() {
+    // named IIFE
+    console.log(`DB CONNECTED `);
+    
+})();
+
+( (name) => {
+    // unnamed IIFE
+    console.log(`DB CONNECTED TWO ${name} `);
+    
+})('unique')
