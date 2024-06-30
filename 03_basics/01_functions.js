a50dae56f7d457f2ed4d9eb31267f8a3b66d4ccb
Unique Jain <unique.21bcon647@jecrcu.edu.in>
2024-06-30 04:53:00 +0000

    functions in JS

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -46,7 +46,37 @@ function loginUserMessage(username = "sam"){
     
 }
 
-console.log(loginUserMessage("unique"));
+// console.log(loginUserMessage("unique"));
 // console.log(loginUserMessage());
 
+// Rest Operator (...num1)
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
 
+console.log(calculateCartPrice(200, 400, 300, 2000));
+
+const user = {
+    username: "Unique",
+    prices: 199,
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user);
+handleObject({
+    username : "sam",
+    price : 399,    
+});
+
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 400, 1000]));
