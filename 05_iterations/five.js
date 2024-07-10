4b9a354f2cba037031373b3594f24bdf8c2ad6aa
Unique Jain <unique.21bcon647@jecrcu.edu.in>
2024-07-10 19:30:55 +0000

    loops

---
+++ 05_iterations/five.js
@@ -0,0 +1,14 @@
+// for each
+
+const coding  = ["js", "rb", "py", "java", "cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+    
+// })
+
+
+coding.forEach ( (item) => {
+    console.log(item);
+    
+})
