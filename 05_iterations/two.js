995a2f0e815fcd8b6cdd84581b5bc2c450a2ebb7
Unique Jain <unique.21bcon647@jecrcu.edu.in>
2024-07-10 17:45:28 +0000

    While Loop in JS

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// While Loop
+
+// let i = 0
+// while (i<=10) {
+//     console.log(`Value of i is ${i}`);
+//     i = i + 2
+
+    
+// }
+
+// let myArray = ["flash", "batman", "Ironman"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);   
+//     arr = arr + 1 
+// }
+
+// Do-While Loop
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score<=10);
