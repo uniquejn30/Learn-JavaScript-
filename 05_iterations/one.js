d096fd9c34e029e57ff9da98628ccaeebe6f4d6f
Unique Jain <unique.21bcon647@jecrcu.edu.in>
2024-07-10 16:06:40 +0000

    Loops In JS

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -11,15 +11,50 @@
 // console.log(element);
 
 
-for (let i = 1; i <= 10; i++) {
-    console.log(`Outer Loops value: ${i}`);
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer Loops value: ${i}`);
     
-    for (let j = 1; j <=10; j++) {
-        // console.log(`Inner Loop value: ${j} and inner loop values: ${i}`);
-        console.log(i + '*' + j + '=' + i*j);
+//     for (let j = 1; j <=10; j++) {
+//         // console.log(`Inner Loop value: ${j} and inner loop values: ${i}`);
+//         console.log(i + '*' + j + '=' + i*j);
         
-    }
+//     }
     
+// }
+ 
+// let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+
+
+// break and continue
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 5){
+        console.log(`Detected 5`);
+        break
+    }
+     console.log(`Value of i is ${i}`);
+     
+    
+}
+
+// Alt+shipt+down arrow key 
+
+for (let i = 1; i <= 20; i++) {
+    if(i == 5){
+        console.log(`Detected 5`);
+        continue
+    }
+     console.log(`Value of i is ${i}`);
 }
 
 
